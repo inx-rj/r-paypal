d2da640d9080c4720407ffd67495505684e8720e
inx-rj <rahulj.inexture@gmail.com>
2023-01-25 11:45:25 +0530

    Paypal setup and added it's component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PayPalScriptProvider options={{ "client-id": "AY6d1oHkK5aVXtoHgJq5ivOLjFSHl51Ugfyt8pRmVeGhmkC_44-4brUoz6ri7swWWgZovJUyrTbIf_3x" }}>
+      <PayPalButtons style={{ layout: "horizontal" }} />
+    </PayPalScriptProvider>
   );
 }
-
-export default App;
